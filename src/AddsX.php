71ad9d7cd5481cd650fe7d849d3e7a7fcbe814e7
Ricardo Boss <mail@ricardoboss.de>
2022-08-12 14:25:35 +0200

    Initial commit

---
+++ src/AddsX.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Elephox\Builder\MyExtension;
+
+trait AddsX
+{
+	// uncomment the functions of WebApplicationBuilder that you need:
+//	abstract protected function getServices(): ServiceCollection;
+//	abstract protected function getPipeline(): RequestPipelineBuilder;
+//	abstract protected function getEnvironment(): WebEnvironment;
+//	abstract protected function getConfigurationBuilder(): ConfigurationBuilder;
+//	abstract public function service(string $name): object;
+//	abstract public function setRequestRouterEndpoint(?RequestRouter $router = null): RequestRouter;
+
+	public function addX(): void
+	{
+	}
+}
